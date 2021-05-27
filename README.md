3b620c47bc647211a32c2de26cd8a1107ca69c3e
Paul Kukowski <paul@debian.g0dnerd.edu>
2021-05-27 10:33:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Game of Life
+
+This is a simple implementation of Conway's Game of Life using pygame as the drawing engine.  
+Press spacebar to advance to the next generation.  
+Default grid size is 50x50 and the default pattern is a 3-period pulsar.
---
+++ game.py
@@ -0,0 +1,230 @@
+import pygame
+from pygame.locals import *
+from random import randrange
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+GREY = (169, 169, 169)
+
+X_SIZE = 500
+Y_SIZE = 500
+OFFSET = 10
+
+pygame.init()
+
+# Set the width and height of the screen [width, height]
+size = (X_SIZE, Y_SIZE)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Conway's Game of Life")
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+
+# gridWidth = X_SIZE / OFFSET
+# gridHeight = Y_SIZE / OFFSET
+gridWidth, gridHeight = 50, 50;
+grid = [[0 for x in range(gridWidth)] for y in range(gridHeight)]
+
+
+# Fill the grid with a random amount (between 120 and 150) of live cells
+# startAmount = randrange(160, 190)
+
+#for i in range(startAmount):
+#	j = randrange(0, gridWidth)
+#	k = randrange(0, gridHeight)
+#	grid[j][k] = 1
+
+grid[4][2] = 1
+grid[5][2] = 1
+grid[6][2] = 1
+grid[10][2] = 1
+grid[11][2] = 1
+grid[12][2] = 1
+
+grid[2][4] = 1
+grid[7][4] = 1
+grid[9][4] = 1
+grid[14][4] = 1
+
+grid[2][5] = 1
+grid[7][5] = 1
+grid[9][5] = 1
+grid[14][5] = 1
+
+grid[2][6] = 1
+grid[7][6] = 1
+grid[9][6] = 1
+grid[14][6] = 1
+
+grid[4][7] = 1
+grid[5][7] = 1
+grid[6][7] = 1
+grid[10][7] = 1
+grid[11][7] = 1
+grid[12][7] = 1
+
+grid[4][9] = 1
+grid[5][9] = 1
+grid[6][9] = 1
+grid[10][9] = 1
+grid[11][9] = 1
+grid[12][9] = 1
+
+grid[2][10] = 1
+grid[7][10] = 1
+grid[9][10] = 1
+grid[14][10] = 1
+
+grid[2][11] = 1
+grid[7][11] = 1
+grid[9][11] = 1
+grid[14][11] = 1
+
+grid[2][12] = 1
+grid[7][12] = 1
+grid[9][12] = 1
+grid[14][12] = 1
+
+grid[4][14] = 1
+grid[5][14] = 1
+grid[6][14] = 1
+grid[10][14] = 1
+grid[11][14] = 1
+grid[12][14] = 1
+
+liveSpaces = list()
+killSpaces = list()
+
+# print(grid)
+
+def get_live_neighbors(grid, x, y):
+	neighbors = 0
+	try:
+		if grid[x+1][y+1] == 1:
+			neighbors += 1
+			# print("Grid at " + str(x) + ", " + str(y) + " neighbor found: " + str(x+1) + ", " + str(y+1))
+	except:
+		pass
+	try:
+		if grid[x-1][y-1] == 1:
+			neighbors += 1
+			# print("Grid at " + str(x) + ", " + str(y) + " neighbor found: " + str(x-1) + ", " + str(y-1))
+	except:
+		pass
+	try:
+		if grid[x-1][y+1] == 1:
+			neighbors += 1
+			# print("Grid at " + str(x) + ", " + str(y) + " neighbor found: " + str(x-1) + ", " + str(y+1))
+	except:
+		pass
+	try:
+		if grid[x+1][y-1] == 1:
+			neighbors += 1
+			# print("Grid at " + str(x) + ", " + str(y) + " neighbor found: " + str(x+1) + ", " + str(y-1))
+	except:
+		pass
+	try:
+		if grid[x+1][y] == 1:
+			neighbors += 1
+			# print("Grid at " + str(x) + ", " + str(y) + " neighbor found: " + str(x+1) + ", " + str(y))
+	except:
+		pass
+	try:
+		if grid[x][y+1] == 1:
+			neighbors += 1
+			# print("Grid at " + str(x) + ", " + str(y) + " neighbor found: " + str(x) + ", " + str(y+1))
+	except:
+		pass
+	try:
+		if grid[x-1][y] == 1:
+			neighbors +=1
+			# print("Grid at " + str(x) + ", " + str(y) + " neighbor found: " + str(x-1) + ", " + str(y))
+	except:
+		pass
+	try:
+		if grid[x][y-1] == 1:
+			neighbors += 1
+			# print("Grid at " + str(x) + ", " + str(y) + " neighbor found: " + str(x) + ", " + str(y-1))
+	except:
+		pass
+
+	return neighbors
+
+while not done:
+
+	space = False
+    # --- Main event loop
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
+
+    # --- Game logic should go here
+
+    # --- Screen-clearing code goes here
+	screen.fill(WHITE)
+
+
+    # Draw the grid
+	drawOffset = OFFSET
+
+	while drawOffset < X_SIZE:
+		pygame.draw.line(screen, GREY,[drawOffset, 0], [drawOffset, Y_SIZE])
+		pygame.draw.line(screen, GREY,[0, drawOffset], [X_SIZE, drawOffset])
+		drawOffset = drawOffset + 10
+
+	# Fill the grid with live cells
+	for i in range(gridWidth):
+		for j in range(gridHeight):
+			if grid[i][j] == 1:
+				pygame.draw.rect(screen, BLACK,(10*i+1, 10*j+1, 8, 8))
+
+
+	# --- Go ahead and update the screen with what we've drawn.
+	pygame.display.flip()
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE:
+				space = True
+
+	if space:
+		for i in range(gridWidth):
+			for j in range(gridHeight):
+				neighbors = get_live_neighbors(grid, i, j)
+				# print("Grid at " + str(i) + ", " + str(j) + " has " + str(neighbors) + " neighbors.")
+				if grid[i][j] == 1:
+					if neighbors < 2:
+						app = (i,j)
+						killSpaces.append(app)
+					if neighbors > 3:
+						app = (i,j)
+						killSpaces.append(app)
+				else:
+					if neighbors == 3:
+						app = (i,j)
+						liveSpaces.append(app)
+
+	for x,y in killSpaces:
+		grid[x][y] = 0
+	for x,y in liveSpaces:
+		grid[x][y] = 1
+
+	killSpaces.clear()
+	liveSpaces.clear()
+
+    # --- Limit to 60 frames per second
+	# clock.tick(60)
+ 
+# Close the window and quit.
+pygame.quit()
